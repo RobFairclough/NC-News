579fc7fab8d25ccc8126840c8918b525c34ea92a
Rob Fairclough <rob.a.fairclough@gmail.com>
2019-01-18 09:15:50 +0000

    remove auths

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -2,4 +2,3 @@ exports.articleData = require('./articles');
 exports.topicData = require('./topics');
 exports.userData = require('./users');
 exports.commentData = require('./comments');
-exports.authData = require('./auths');
