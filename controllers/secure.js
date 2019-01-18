0c38ae11081b06321a0931e3ca5d1394ccee15de
Rob Fairclough <rob.a.fairclough@gmail.com>
2019-01-18 14:11:19 +0000

    optional security

--- controllers/secure.js
+++ controllers/secure.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = require('../passconfig');
 
 const authorise = (req, res, next) => {
+  // return next();
   const { authorization } = req.headers;
   if (!authorization) next({ status: 401, msg: 'Unauthorised' });
   else {
