73698afd12a76e1dea1c6d5b4359496cdfb5f9c3
Rob Fairclough <rob.a.fairclough@gmail.com>
2019-02-08 14:11:56 +0000

    testing for article issue

--- controllers/articles.js
+++ controllers/articles.js
@@ -24,7 +24,7 @@ const sendAllArticles = (req, res, next) => {
     .offset(offset)
     .limit(limit)
     .count('comments.comment_id AS comment_count')
-    .groupBy('articles.article_id', 'articles.username')
+    .groupBy('articles.article_id', 'articles.username', 'users.username')
     .then((articles) => {
       reformatDate(articles);
       res.send({ articles });
